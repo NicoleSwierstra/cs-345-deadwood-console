32a9700b79f8463d6bb21b65acaacd45b9bab191
DaniilBolgar <44933391+DaniilBolgar@users.noreply.github.com>
2026-02-14 21:38:05 -0800

    SceneCard constructor + toString

--- Deadwood/SceneCard.cs
+++ Deadwood/SceneCard.cs
@@ -3,4 +3,16 @@ class SceneCard
     public string name;
     public string desc;
     private Role[] roles;
+
+    public SceneCard(string name, string desc, Role[] roles)
+    {
+        this.name = name;
+        this.desc = desc;
+        this.roles = roles;
+    }
+
+    public string toString()
+    {
+        return "{\"" +name + ": " + desc + "\"}";
+    }
 }
